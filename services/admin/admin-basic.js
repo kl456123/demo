70712eb9ba54f43a4e533dccbab6762c50b76079
wyr <1293124878@qq.com>
2015-08-26 20:52:41 +0800

    添加了管理端member和basic页面的分页。 #3

--- services/admin/admin-basic.js
+++ services/admin/admin-basic.js
@@ -2,6 +2,7 @@
 var ligle = require('../index.js').ligle;
 var logger = ligle.util.logger('basic');
 var Model = require('../model/basic.js');
+var pageCalculate = require('../common').pageCalculate;
 
 // 路由
 var router = ligle.base.routes.Router(Model);
@@ -54,8 +55,14 @@ router
     logger.debug('/a_basic','get');
     var rd = res.ligle.renderer;
     var obj = res.ligle.model;
-    obj.getList({},function(err,objs){
-      rd.render('console/a_basic',{basic:objs});
+    var curPage = req.query.page?parseInt(req.query.page):1;
+    obj.count({}, function(err, totalNum){
+      if(err) totalNum = 0;
+      var pObj = pageCalculate(curPage, totalNum);
+      logger.trace('pObj:'+ pObj);
+      obj.getList({},{sort:{_time: -1}, skip:pObj.skipNum, limit:pObj.limitNum}, function(err,objs){
+        rd.render('console/a_basic',{basic:objs, curPage:pObj.curPage, totalPage:pObj.totalPage});
+      });
     });
   });
 
