428f89fe06dece77c4a889b57677312e9037d80f
wyr <1293124878@qq.com>
2015-09-10 20:47:13 +0800

    富文本提取图片地址 有报错  #14

--- services/client/richText.js
+++ services/client/richText.js
@@ -3,6 +3,7 @@ var app = require('../../app.js');
 var ligle = require('../index.js').ligle;
 var Model = require('../model/richText.js');
 var logger = ligle.util.logger('richText');
+var basicModel = require('../model/basic.js');
 
 var router = app.Router();
 
@@ -25,8 +26,59 @@ router
     return res.send(filePaths);
   })
 
+router
+  .route('/test')
+  .get(function(req, res){
+    var imgTags = getImgTags(basicModel);
+    logger.trace('1111111111111111111111111111');
+    logger.trace('imgTags: ', imgTags);
+    var imgPaths = getImgPaths(imgTags);
+    logger.trace(imgPaths);
+    res.end();
+  });
 
 module.exports = router;
 
 
+//[{model: Model, richTextName: textarea}, {}, ]
 
+var getImgTags = function(Model){
+  var obj = new Model();
+  var imgHtmlReg = /<img[^>]*>/g;      //匹配img标签 <img ...>
+  var imgTags = [];
+  obj.getList({textarea: imgHtmlReg}, function(err, objs){ //匹配出所有含img的条目
+    objs.forEach(function(o, i){       //textarea: "<p><img src= ><p>..."
+      var imgTagsTemp = o.textarea.match(imgHtmlReg);   //[ <img >, <img>,  ]
+      logger.trace('imgTagsTemp: ', imgTagsTemp);
+      for(var j = 0;j<imgTagsTemp.length;j++){
+        imgTags.push(imgTagsTemp[j]);
+      }
+
+    });
+    logger.trace(imgTags);
+    return imgTags;
+  });
+}
+
+var getImgPaths = function(imgTags){
+  logger.trace('2222222222222222222222222222');
+  var imgPaths = [];
+  for(var i=0;i<imgTags.length;i++){
+    var temp = getPath(imgTags[i]);
+    imgPaths.push(temp);
+  }
+  logger.trace(imgPaths);
+}
+
+var getPath = function(o){
+  var imgSrcReg = /src=\"([^\s\"]*)\"/g;
+  var matched = o.match(imgSrcReg);
+  if(matched === null) return;
+  matched.forEach(function(m, i){
+    return m.replace(imgSrcReg,"\"$1\"");
+  });
+}
+
+var removeRichTextImg = function(paths){
+
+}
