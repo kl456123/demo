80906349cad0d903f60c08e7a9d441efe5a9cb3f
wyr <1293124878@qq.com>
2015-09-16 16:56:23 +0800

    富文本图片处理（没有自动保存部分）  #14

--- services/client/richText.js
+++ services/client/richText.js
@@ -3,10 +3,7 @@ var app = require('../../app.js');
 var ligle = require('../index.js').ligle;
 var Model = require('../model/richText.js');
 var logger = ligle.util.logger('richText');
-var basicModel = require('../model/basic.js');
 var fs = require('fs');
-var moment = require('moment');
-var async = require('async');
 
 var router = app.Router();
 
@@ -17,80 +14,32 @@ router
     var obj = new Model();
     var filePaths = [];
     logger.trace('req.files: ', req.files);
-    obj.processFiles(req.files);
-    if(obj.file instanceof Array){
-      obj.file.forEach(function(o, i){
-        filePaths.push(o.path);
-      });
-    }else{
-      filePaths.push(obj.file.path);
+
+    //create temp文件夹
+    var tempUpDir = obj.getUploadDir() + 'temp';
+    var tempHostDir = obj.getHostDir() + 'temp';
+    if(!fs.existsSync(tempUpDir)) fs.mkdirSync(tempUpDir);
+
+    //save temp pics to temp directory
+    var fieldname = 'file';
+    var filelist;
+    if(!req.files[fieldname]){
+      return res.send(new Error('wrong'));
     }
+    if(req.files[fieldname] instanceof Array){
+      filelist = req.files[fieldname];
+    }else{
+      filelist = [req.files[fieldname]];
+    }
+    filelist.forEach(function(o,i){
+      var newPath = tempUpDir+'/'+o.name; // 文件实际保存的path
+      var showPath = tempHostDir+'/'+o.name;// 渲染模板用的path
+      fs.renameSync(o.path,newPath);
+      filePaths.push(showPath);
+    });
+
     logger.trace('filePaths: ', filePaths);
     return res.send(filePaths);
   })
 
-
-router
-  .route('/test')
-  .get(function(req, res){
-    clearImgPath([basicModel]);
-    res.end();
-  });
-
-
 module.exports = router;
-
-
-//[{model: Model, richTextName: textarea}, {}, ]
-
-var clearImgPath = function(ModelArray){
-  async.map(ModelArray, getImgPaths, function(err, results){
-    if(err) results = [];
-    logger.trace('results: ', err, results[0]);
-    removeRichTextImg(results[0]);
-  });
-};
-
-var getImgPaths = function(Model, cb){
-  var obj = new Model();
-  var imgHtmlReg = /<img[^>]*>/g;
-  var imgPaths = [];
-  obj.getList({textarea: imgHtmlReg}, function(err, objs){ //匹配出所有含img的条目
-    objs.forEach(function(o, i){
-      var imgTags = o.textarea.match(imgHtmlReg);  //[]
-      logger.trace('imgTags: ', imgTags);
-      for(var j = 0;j<imgTags.length;j++){
-        var tempImgPath = getPath(imgTags[j]);   //获取所有<img>标签的src路径
-        imgPaths.push(tempImgPath);
-      };
-    });
-    //logger.trace(imgTags);
-    cb(null, imgPaths);
-  });
-};
-
-var getPath = function(path){
-  var imgSrcReg = /src=\"([^\s\"]*)\"/g;
-  var matched = path.match(imgSrcReg);
-  if(matched === null) return;
-  var path = matched[0].replace(imgSrcReg,"$1");
-  //logger.trace('path: ', path);
-  return path;
-}
-
-
-var removeRichTextImg = function(paths){
-  var obj = new Model();
-  var upLoadDir = obj.getUploadDir();
-  logger.trace('uploadDir: ', upLoadDir);
-  fs.readdir(upLoadDir, function(err, files){
-    logger.trace('files: ',files);
-    files.forEach(function(filename, i){
-      var temp = obj.upload2HostPath(upLoadDir)+filename;
-      logger.trace('temp: ', temp);
-      if(paths.indexOf(temp) === -1){
-        fs.unlinkSync(upLoadDir+filename);
-      }
-    });
-  });
-}
