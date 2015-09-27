1b852d6a3e804cda2d11203d49848d1953ef2946
crackhopper <710591209@qq.com>
2015-09-27 13:50:10 +0800

    add phantomjs for testing #29

---
+++ test/phantomjs_test.js
@@ -0,0 +1,31 @@
+var driver = require('node-phantom-simple');
+
+driver.create({ path: require('phantomjs').path }, function (err, browser) {
+  return browser.createPage(function (err, page) {
+    return page.open("http://tilomitra.com/repository/screenscrape/ajax.html", function (err,status) {
+      console.log("opened site? ", status);
+      page.includeJs('http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js', function (err) {
+        // jQuery Loaded.
+        // Wait for a bit for AJAX content to load on the page. Here, we are waiting 5 seconds.
+        setTimeout(function () {
+          return page.evaluate(function () {
+            //Get what you want from the page using jQuery. A good way is to populate an object with all the jQuery commands that you need and then return the object.
+            var h2Arr = [],
+                pArr = [];
+
+            $('h2').each(function () { h2Arr.push($(this).html()); });
+            $('p').each(function () { pArr.push($(this).html()); });
+
+            return {
+              h2: h2Arr,
+              p: pArr
+            };
+          }, function (err,result) {
+            console.log(result);
+            browser.exit();
+          });
+        }, 5000);
+      });
+      });
+  });
+});
